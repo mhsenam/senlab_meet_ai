311e0ed2b13e78dbfc12eb76e0c1e7a5aa76d8b6
mhsenam <mohsenamini1081@gmail.com>
2025-06-25 09:53:15 +0330

    Add user authentication and database schema
    - Introduced `better-auth` for user authentication in package.json and package-lock.json.
    - Updated Home component in page.tsx to include a sign-up form with email, password, and name fields.
    - Refactored database schema in schema.ts to define user, session, account, and verification tables for authentication.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,51 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email(
+      {
+        email,
+        name,
+        password,
+      },
+      {
+        onError: (error) => {
+          window.alert(error.error.message);
+        },
+        onSuccess: () => {
+          window.alert("success");
+        },
+      }
+    );
+  };
+
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="flex flex-col gap-4 m-4">
+      <Input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Input
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Button onClick={onSubmit}>Sign up</Button>
     </div>
   );
 }
